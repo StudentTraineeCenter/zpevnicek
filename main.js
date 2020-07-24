81c94c9e5b55c4c261d279d64db50c01b7be7393
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-24 17:19:58 +0200

    Optimize main.js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 window.onload = function chordFromSuperMusic() {
     let str = document.getElementById("textSongu").innerHTML;
-        for (let index = 0; index < str.length; index++) {
-            let c = str[index];
-            if (c == "[") {
-                str = str.replace("[", "<em>");
-            }else if (c == "]") {
-                str = str.replace("]", "</em>");
-            }   
-        }
-        document.getElementById("textSongu").innerHTML = str;
+    str.split("[").join("<em>")
+    str.split("]").join("</em>")
+    document.getElementById("textSongu").innerHTML = str;
 }
-
