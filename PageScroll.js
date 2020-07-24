bbb6af5e5e8a999759d50353bf04f937fc5090b9
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-24 17:15:43 +0200

    Format

--- PageScroll.js
+++ PageScroll.js
@@ -4,46 +4,46 @@
 //For full source code and more DHTML scripts, visit https://www.dynamicdrive.com
 //This credit MUST stay intact for use
 
-var Hoffset=70 //Enter buttons' offset from right edge of window (adjust depending on images width)
-var Voffset=80 //Enter buttons' offset from bottom edge of window (adjust depending on images height)
-var thespeed=1 //Enter scroll speed in integer (Advised: 1-3)
+var Hoffset = 70 //Enter buttons' offset from right edge of window (adjust depending on images width)
+var Voffset = 80 //Enter buttons' offset from bottom edge of window (adjust depending on images height)
+var thespeed = 1 //Enter scroll speed in integer (Advised: 1-3)
 
-var ieNOTopera=document.all&&navigator.userAgent.indexOf("Opera")==-1
-var myspeed=0
+var ieNOTopera = document.all && navigator.userAgent.indexOf("Opera") == -1
+var myspeed = 0
 
-var ieHoffset_extra=document.all? 15 : 0
-var cross_obj=document.all? document.all.staticbuttons : document.getElementById? document.getElementById("staticbuttons") : document.staticbuttons
+var ieHoffset_extra = document.all ? 15 : 0
+var cross_obj = document.all ? document.all.staticbuttons : document.getElementById ? document.getElementById("staticbuttons") : document.staticbuttons
 
-function iecompattest(){
-return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+function iecompattest() {
+    return (document.compatMode && document.compatMode != "BackCompat") ? document.documentElement : document.body
 }
 
-function positionit(){
-var dsocleft=document.all? iecompattest().scrollLeft : pageXOffset
-var dsoctop=document.all? iecompattest().scrollTop : pageYOffset
-var window_width=ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra
-var window_height=ieNOTopera? iecompattest().clientHeight : window.innerHeight
+function positionit() {
+    var dsocleft = document.all ? iecompattest().scrollLeft : pageXOffset
+    var dsoctop = document.all ? iecompattest().scrollTop : pageYOffset
+    var window_width = ieNOTopera ? iecompattest().clientWidth + ieHoffset_extra : window.innerWidth + ieHoffset_extra
+    var window_height = ieNOTopera ? iecompattest().clientHeight : window.innerHeight
 
-if (document.all||document.getElementById){
-cross_obj.style.left=parseInt(dsocleft)+parseInt(window_width)-Hoffset+"px"
-cross_obj.style.top=dsoctop+parseInt(window_height)-Voffset+"px"
-}
-else if (document.layers){
-cross_obj.left=dsocleft+window_width-Hoffset
-cross_obj.top=dsoctop+window_height-Voffset
-}
-}
-
-function scrollwindow(){
-window.scrollBy(0,myspeed)
+    if (document.all || document.getElementById) {
+        cross_obj.style.left = parseInt(dsocleft) + parseInt(window_width) - Hoffset + "px"
+        cross_obj.style.top = dsoctop + parseInt(window_height) - Voffset + "px"
+    }
+    else if (document.layers) {
+        cross_obj.left = dsocleft + window_width - Hoffset
+        cross_obj.top = dsoctop + window_height - Voffset
+    }
 }
 
-function initializeIT(){
-positionit()
-if (myspeed!=0){
-scrollwindow( );
-}
+function scrollwindow() {
+    window.scrollBy(0, myspeed)
 }
 
-if (document.all||document.getElementById||document.layers)
-setInterval("initializeIT()",70)
+function initializeIT() {
+    positionit()
+    if (myspeed != 0) {
+        scrollwindow();
+    }
+}
+
+if (document.all || document.getElementById || document.layers)
+    setInterval("initializeIT()", 70)
