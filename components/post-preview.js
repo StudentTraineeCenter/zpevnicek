79e39a5d457978666c4cfe0c81efdc43ddccf388
Filip Troníček <filip.tronicek@seznam.cz>
2021-06-22 19:05:55 +0000

    Some styling + tailwind JIT!

--- components/post-preview.js
+++ components/post-preview.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function PostPreview({ name, author, slug }) {
   return (
-    <li>
+    <div className="">
       <Link as={`/posts/${slug}`} href="/posts/[slug]">
-        <a className="text-3xl hover:underline">
-          - Píseň {name} - {author}
+        <a className="text-3xl hover:underline text-[#0078d7]">
+          Píseň {name} - {author}
         </a>
       </Link>
-    </li>
+    </div>
   );
 }
