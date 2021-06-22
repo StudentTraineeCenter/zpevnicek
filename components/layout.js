60d304842ba436dc23cacfda436955c99c5f6469
Filip Troníček <filip.tronicek@seznam.cz>
2021-06-22 18:02:15 +0000

    Remove some stuff

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Footer from '../components/footer'
 import Meta from '../components/meta'
 
-export default function Layout({ preview, children }) {
+export default function Layout({ children }) {
   return (
     <>
       <Meta />
