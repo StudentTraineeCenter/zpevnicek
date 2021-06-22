eafd256f824d2e089a04f26b2a9783e78ffaba79
Petr Kučera <petr@khome.cz>
2021-06-22 17:53:11 +0000

    layout for form

---
+++ pages/form.js
@@ -0,0 +1,18 @@
+import Layout from "../components/layout";
+import Head from 'next/head'
+import Container from "../components/container";
+
+export default function Form() {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>Formular pro expot pisni</title>
+                </Head>
+                <Container>
+                    
+                </Container>
+            </Layout>
+        </>
+    )
+}
