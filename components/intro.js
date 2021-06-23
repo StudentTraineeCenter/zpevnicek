2525e2704db032994c75c92e969a36c9aa758c32
Filip Troníček <filip.tronicek@seznam.cz>
2021-06-23 14:03:07 +0000

    Add a link to the form

--- components/intro.js
+++ components/intro.js
@@ -1,5 +1,3 @@
-import { CMS_NAME } from '../lib/constants'
-
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -10,5 +8,5 @@ export default function Intro() {
         Písně k ohni, pivečku a dobré náladě
       </h4>
     </section>
-  )
+  );
 }
