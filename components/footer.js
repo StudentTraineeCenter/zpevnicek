dd2915c806b2c0d7d8e8fd324c72284ca5a8d7f4
Filip Troníček <filip.tronicek@seznam.cz>
2021-06-23 19:07:02 +0000

    © Disclaimer in the footer
    fixes #12

--- components/footer.js
+++ components/footer.js
@@ -5,9 +5,40 @@ export default function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2 mt-4">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">STC Zpěvníček</h3>
+          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            STC Zpěvníček
+          </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            Vytvořeno Péťou a&nbsp;<a className="hover:underline" target="_blank" rel="noreferrer noopener" href="https://trnck.dev/">Fílou</a>&nbsp;s láskou na&nbsp;<a className="hover:underline" target="_blank" rel="noreferrer noopener" href="https://github.com/microsoftstc/zpevnicek">GitHubu</a>
+            <span>
+              <span>
+                {" "}
+                <strong>Disclaimer:</strong> Všechny texty písniček jsou dílem
+                příslušných autorů. Nepřisvojujeme si k nim vlastnictví ani
+                žádná jiná práva.
+              </span>
+              <br />
+              <br />
+              Vytvořeno Péťou a&nbsp;
+              <a
+                className="hover:underline"
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://trnck.dev/"
+              >
+                Fílou
+              </a>
+              &nbsp;s láskou na&nbsp;
+              <a
+                className="hover:underline"
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://github.com/microsoftstc/zpevnicek"
+              >
+                GitHubu
+              </a>
+            </span>
+            <br />
+            <span className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2"></span>
           </div>
         </div>
       </Container>
