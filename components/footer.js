6caa146b3a2af9f690c3effd0032a84fa16485b0
Filip Troníček <filip.tronicek@seznam.cz>
2021-06-23 13:31:30 +0000

    Update the footer

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,13 @@
 import Container from "./container";
-import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
+          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">STC Zpěvníček</h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            ( ͡° ͜ʖ ͡°)
+            Vytvořeno Péťou a&nbsp;<a className="hover:underline" target="_blank" rel="noreferrer noopener" href="https://trnck.dev/">Fílou</a>&nbsp;s láskou na&nbsp;<a className="hover:underline" target="_blank" rel="noreferrer noopener" href="https://github.com/microsoftstc/zpevnicek">GitHubu</a>
           </div>
         </div>
       </Container>
