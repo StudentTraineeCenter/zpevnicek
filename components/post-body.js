99ae7f720fce08c6bbe60d488ffb152613f2c757
Filip Troníček <filip.tronicek@seznam.cz>
2021-06-25 08:17:34 +0200

    Author posts view (#16)

--- components/post-body.js
+++ components/post-body.js
@@ -1,23 +1,30 @@
+import Link from "next/link";
+
 export default function PostBody({ content, note, author }) {
-    content = content.split("[").join('<em class="chord">');
-    content = content.split("]").join("</em>");
-    return (
-        <div className="max-w-2xl mx-auto">
-            <div className="dark:bg-gray-700 bg-gray-200 text-3xl px-4 py-6">
-                {author && (
-                    <div className="mb-3">
-                        <span title="Autor">👤:</span> {author}
-                    </div>
-                )}
-                {note && (
-                    <div className="mt-2">
-                        <span title="Poznámka">📝:</span> {note}
-                    </div>
-                )}
+  content = content.split("[").join('<em class="chord">');
+  content = content.split("]").join("</em>");
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div className="dark:bg-gray-700 bg-gray-200 text-3xl px-4 py-6">
+        {author && (
+          <Link href={`/author/${author}`}>
+            <div className="mb-3 cursor-pointer">
+              <span title="Autor">👤:</span> {author}
             </div>
-            <div className="text-xl mt-12 song-text" dangerouslySetInnerHTML={{
-                __html: content
-            }}></div>
-        </div>
-    );
+          </Link>
+        )}
+        {note && (
+          <div className="mt-2">
+            <span title="Poznámka">📝:</span> {note}
+          </div>
+        )}
+      </div>
+      <div
+        className="text-xl mt-12 song-text"
+        dangerouslySetInnerHTML={{
+          __html: content,
+        }}
+      ></div>
+    </div>
+  );
 }
---
+++ pages/author/[author].js
@@ -0,0 +1,60 @@
+import Container from "../../components/container";
+import MoreStories from "../../components/more-stories";
+import Header from "../../components/header";
+import Intro from "../../components/intro";
+import Layout from "../../components/layout";
+import { getAllPosts } from "../../lib/api";
+import Head from "next/head";
+
+export default function Author({ allPosts, author }) {
+  const filteredPosts = allPosts.filter((post) => post.author === author);
+
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>{author} | STC Zpěvníček</title>
+        </Head>
+        <Container>
+          <Header />
+          {allPosts.length > 0 && (
+            <MoreStories author={author} posts={filteredPosts} />
+          )}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const author = params.author;
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  return {
+    props: {
+      author,
+      allPosts,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts();
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          author: post.author,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
