f333949d25120e8142068217bf8ce9d3a3415df4
Petr Kučera <petr@khome.cz>
2021-06-29 07:43:55 +0000

    create file with basic strucuture

---
+++ components/transposition-controller.js
@@ -0,0 +1,12 @@
+export default function TranspositionController(){
+    return (
+        <div>
+            <div>
+                <h2>Transpozice</h2>
+            </div>
+            <div>
+                <h2>Velikost písma</h2>
+            </div>
+        </div>
+    )
+}
