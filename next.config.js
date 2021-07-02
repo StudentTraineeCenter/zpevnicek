f4c3e5e39158f8a92d58e61903b402aa0a29a4e7
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-02 22:41:46 +0200

    Add aggressive caching for all songs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        disable: process.env.NODE_ENV === 'development',
-    }
+  async headers() {
+    return [
+      {
+        source: "/song/:slug*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=604800, immutable",
+          },
+        ],
+      },
+    ];
+  },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
 });
