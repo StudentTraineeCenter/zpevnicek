0674c54f2380eaa78fb15ed981cba1c76c9a5e3d
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-02 22:57:02 +0200

    Update post-header.js

--- components/post-header.js
+++ components/post-header.js
@@ -1,11 +1,9 @@
-import PostTitle from '../components/post-title'
+import PostTitle from "../components/post-title";
 
 export default function PostHeader({ name }) {
   return (
     <>
       <PostTitle>🎵 {name}</PostTitle>
-      <div className="max-w-2xl mx-auto">
-      </div>
     </>
-  )
+  );
 }
