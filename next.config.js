e907cc0ce895decabb59cf3d70a76e8f4420aaf4
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-03 17:16:35 +0200

    Add more options to Cache-Control
    Add s-maxage, stale-revalidate and stale-if-error

--- next.config.js
+++ next.config.js
@@ -7,9 +7,15 @@ module.exports = withPWA({
       {
         source: "/song/:slug*",
         headers: [
+          /*
+            s-maxage=2419200, // Set the max age of the cached content to a month (28 days)
+            stale-while-revalidate=1209600, // Stale whhile revalidate for a fortnight
+            stale-if-error=31536000`, // If an error occurs with the revalidation of the cached content, then the cached content will be served for a maximum of a year (365 days)
+          */
           {
             key: "Cache-Control",
-            value: "public, max-age=604800",
+            value:
+              "public,s-maxage=2419200,stale-while-revalidate=1209600,stale-if-error=31536000",
           },
         ],
       },
