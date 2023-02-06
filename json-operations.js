a9915f38314ebfeb6cdbbc9a253d6ad2d2524450
Petr Kucera <petr@khome.cz>
2023-02-06 15:52:00 +0100

    Create the prebuild script

--- json-operations.js
+++ json-operations.js
@@ -18,18 +18,39 @@ class MetaSong {
  * of the index page.
  */
 function generateMetaJson() {
+  console.log(
+    "~~~~~\nThe function for generating JSON file with song's metadata has been started!"
+  );
+
   const files = fs.readdirSync("_songs");
   let out = new Array();
   files.map((file) => {
     const song = JSON.parse(fs.readFileSync(`_songs/${file}`));
     out.push(new MetaSong(song.name, song.slug, song.author));
-    // fs.writeFileSync(
-    //   "_songs-metadata.json",
-    //   JSON.stringify(new MetaSong(song.name, song.slug, song.author)) + ",",
-    //   { flag: "a+" }
-    // );
   });
   fs.writeFileSync("_songs-metadata.json", JSON.stringify(out));
+  console.log(
+    "The function for generating JSON file with song's metadata succesful end!\n~~~~~"
+  );
 }
 
-generateMetaJson();
+function printManual() {
+  console.log(`
+    MANUAL FOR JSON OPERATIONS
+    -m  run the function generates the JSON file, which contains only the song's
+        metadata without page content to reduce the loading performance
+        of the index page
+    -h  print manual
+  `);
+}
+
+if (process.argv[2] === "-m") {
+  generateMetaJson();
+  process.exit(0);
+} else if (process.argv[2] === "-h") {
+  printManual();
+  process.exit(0);
+} else {
+  printManual();
+  process.exit(1);
+}
