45a8e120412c379942c407aefec476ed14332524
Majkey25 <matej.teply25@email.cz>
2023-03-08 19:59:47 +0100

    Base look for 404 site

--- pages/404.js
+++ pages/404.js
@@ -8,9 +8,19 @@ export default function Page404() {
       <Layout>
         <Head>
           <title>ERROR 404</title>
-          <p class= "text-center">Page not found</p>
+          
         </Head>
-        <div>ERROR 404</div> 
+        
+      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-800 py-6 sm:py-12">
+  <div class="relative bg-blue-500 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+ <h1 class="box-decoration-clone bg-red-600 text-white font-bold text-2xl text-center sm:rounded-lg ">
+  Error</h1>
+   <h1 class="font-bold text-center text-2xl">404</h1>
+  <p> ------------------------------------------------</p>
+   <p class= "text-center text-xl">Page not foud </p>
+  </div>
+</div>
+
         <Container>
         
         </Container>
