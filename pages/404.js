7d58634aee0be25ee304b9c1cea8854d38a511a6
Majkey25 <matej.teply25@email.cz>
2023-03-08 21:00:50 +0100

    Another upgrade

--- pages/404.js
+++ pages/404.js
@@ -7,20 +7,17 @@ export default function Page404() {
     <>
       <Layout>
         <Head>
-          <title>ERROR 404</title>
-          
-        </Head>
-        
-      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-800 py-6 sm:py-12">
-  <div class="relative bg-blue-500 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
- <h1 class="box-decoration-clone bg-red-600 text-white font-bold text-2xl text-center sm:rounded-lg ">
-  Error</h1>
-   <h1 class="font-bold text-center text-2xl">404</h1>
-  <p> ------------------------------------------------</p>
-   <p class= "text-center text-xl">Page not foud </p>
+          <title>ERROR 404</title>     
+        </Head>        
+      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-[#24292E] py-6 sm:py-12">
+  <div class="relative bg-[#0078D4] px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+ <h1 class="box-decoration-clone text-white font-bold text-4xl align-middle text-center sm:rounded-lg ">Něco se pokazilo :(  </h1>
+   <h1 class="font-bold text-center text-2xl">Error 404</h1>  
+   <br></br>
+  <div class="flex-grow h-px bg-[#24292E]"></div>
+   <p class= "text-center text-xl" > Page not found </p>
   </div>
 </div>
-
         <Container>
         
         </Container>
